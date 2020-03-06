4523a59208c799047adf738829473c3b26c158aa
David Buehler <creighti@cr8e.net>
2020-03-05 20:57:05 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
+
+namespace commandline_parsing_example
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var configBuilder = new ConfigurationBuilder();
+
+             var switches = new Dictionary<string, string>()
+             {
+                 { "-f", "foo" },
+                 { "--foo", "foo" }
+             };
+
+            // Last one wins, so put lower-priority sources first!
+            configBuilder.AddJsonFile("resources/config.json");
+            configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configBuilder.AddIniFile("resources/config.ini");
+            configBuilder.AddCommandLine(args, switches);
+
+            var config = configBuilder.Build();
+
+            Console.WriteLine("--foo = {0}", config["foo"]);
+            Console.WriteLine("ini file value asection:foo = {0}", config["asection:foo"]);
+            Console.WriteLine("--bar = {0}", config["bar"]);
+        }
+    }
+}
